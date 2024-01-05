d0f7d13baad2f7200927ff5ddc757fa9d639eb26
Hrishikesh Thombare <hrishithombare15@gmail.com>
2024-01-05 22:09:35 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -139,12 +139,9 @@ def handleIndex(index, dic):
 def get_progress_bar_string(pct):
     pct = float(str(pct).strip('%'))
     p = min(max(pct, 0), 100)
-    cFull = int(p // 8)
-    cPart = int(p % 8 - 1)
-    p_str = '■' * cFull
-    if cPart >= 0:
-        p_str += ['▤', '▥', '▦', '▧', '▨', '▩', '■'][cPart]
-    p_str += '□' * (12 - cFull)
+    cFull = int(p // 10)
+    p_str = '★' * cFull
+    p_str += '☆' * (10 - cFull)
     return f"[{p_str}]"
 
 
