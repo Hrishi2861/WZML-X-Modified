eac6741aea81496fb55bac6e8644e33743ac1dbb
Hrishikesh Thombare <hrishithombare15@gmail.com>
2024-06-12 08:55:11 +0530

    ADDED STICKERS AND MODIFIED BY @hrishikesh2861

--- bot/__init__.py
+++ bot/__init__.py
@@ -739,8 +739,8 @@ if ospath.exists('shorteners.txt'):
             if len(temp) == 2:
                 shorteners_list.append({'domain': temp[0],'api_key': temp[1]})
 
-if BASE_URL:
-    Popen(f"gunicorn web.wserver:app --bind 0.0.0.0:{BASE_URL_PORT} --worker-class gevent", shell=True)
+PORT = environ.get('PORT')
+Popen(f"gunicorn web.wserver:app --bind 0.0.0.0:{PORT} --worker-class gevent", shell=True)
 
 bot_cache['pkgs'] = ['zetra', 'xon-bit', 'ggrof', 'cross-suck', 'zetra|xon-bit|ggrof|cross-suck']
 
