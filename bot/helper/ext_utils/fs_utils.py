6855aeb75116d4a2e882287fdde15c64f6ca2cf1
Hrishikesh <hrishithombare15@gmail.com>
2025-01-09 09:52:23 +0530

    Fixed Metadata

--- bot/helper/ext_utils/fs_utils.py
+++ bot/helper/ext_utils/fs_utils.py
@@ -178,7 +178,7 @@ async def join_files(path):
                     await aioremove(f'{path}/{file_}')
 
 async def edit_metadata(listener, base_dir: str, media_file: str, outfile: str, metadata: str = ''):
-    cmd = [bot_cache['pkgs'][2], '-hide_banner', '-loglevel', 'error', '-ignore_unknown', '-i', media_file, '-metadata', f'title={metadata}',
+    cmd = ['ffmpeg', '-hide_banner', '-loglevel', 'error', '-ignore_unknown', '-i', media_file, '-metadata', f'title={metadata}',
            '-metadata:s:v', f'title={metadata}', '-metadata', 'Comment=', '-metadata', 'Copyright=', '-metadata', f'AUTHOR=Lord Puneet 👑', '-metadata', 'Encoded by=', '-metadata', 'SYNOPSIS=', '-metadata', 'ARTIST=', '-metadata', 'PURL=', '-metadata', 'Encoded_by=', '-metadata', 'Description=', '-metadata', 'description=', '-metadata', 'SUMMARY=', '-metadata', 'WEBSITE=', '-metadata:s:a', f'title={metadata}',
            '-metadata:s:s', f'title={metadata}', '-map', '0:v:0?', '-map', '0:a:?', '-map', '0:s:?', '-c:v', 'copy', '-c:a', 'copy', '-c:s',
            'copy', outfile, '-y']
