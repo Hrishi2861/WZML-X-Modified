acf923ad354d4c3a101ec0b7d04af42752bd1522
Hrishikesh <hrishithombare15@gmail.com>
2026-04-24 14:14:20 +0530

    Minor Changes

--- bot/helper/languages/en.py
+++ bot/helper/languages/en.py
@@ -1,5 +1,5 @@
 START_MSG = """
-This bot can mirror all your links|files|torrents to Google Drive or any rclone cloud or to telegram or to ddl servers. Repo used is <a href='https://github.com/SilentDemonSD/WZML-X>WZML-X</a>. And done some <a href='https://github.com/Hrishi2861/WZML-X-Modified>Modifications</a> ❤️.
+This bot can mirror all your links|files|torrents to Google Drive or any rclone cloud or to telegram or to ddl servers. Repo used is <a href='https://github.com/SilentDemonSD/WZML-X'>WZML-X</a>. And done some <a href='https://github.com/Hrishi2861/WZML-X-Modified'>Modifications</a> ❤️.
 Type /{cmd} to get a list of available commands
 """
 START_BUTTON1 = "☀️ OWNER ☀️"
